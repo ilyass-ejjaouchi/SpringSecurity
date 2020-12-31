6c0863ae93a7fc94358f93ef1f3c51df22849378
ilyass <ilyass.ezzaouchi@gmail.com>
2020-12-31 16:10:57 +0100

    first commit

---
+++ src/main/java/com/tpspringsecurity/Controllers/main.java
@@ -0,0 +1,22 @@
+package com.tpspringsecurity.Controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class main {
+
+    public static void main(String[] args) {
+        String pass = "ilyass123";
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String r1 = encoder.encode(pass);
+        String r2 = encoder.encode(pass);
+        String r3 = encoder.encode(pass);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(encoder.matches(pass,r1));
+        System.out.println(encoder.matches(pass,r2));
+        System.out.println(encoder.matches(pass,r3));
+    }
+}
